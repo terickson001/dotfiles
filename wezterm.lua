8f0bbaf383fee7134f98d4813431afd47503963c
terickson001 <me@tylererickson.com>
2026-06-11 14:39:14 -0700

    Added wezterm.lua

---
+++ wezterm.lua
@@ -0,0 +1,37 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+local function kanagawa(config)
+	config.force_reverse_video_cursor = true
+	config.colors = {
+		foreground = "#dcd7ba",
+		background = "#1f1f28",
+
+		cursor_bg = "#c8c093",
+		cursor_fg = "#c8c093",
+		cursor_border = "#c8c093",
+
+		selection_fg = "#c8c093",
+		selection_bg = "#2d4f67",
+
+		scrollbar_thumb = "#16161d",
+		split = "#16161d",
+
+		ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
+		brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
+		indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
+	}
+	return config
+end
+
+config = kanagawa(config)
+config.window_decorations = "NONE"
+config.font = wezterm.font("Iosevka Term", { weight = "Regular" })
+config.hide_tab_bar_if_only_one_tab = true
+config.default_prog = { "wsl.exe", "--cd", "~", "--user", "tyler" }
+config.freetype_load_flags = "DEFAULT"
+config.window_close_confirmation = "NeverPrompt"
+config.window_background_opacity = 0.6
+config.enable_kitty_keyboard = false
+
+return config
